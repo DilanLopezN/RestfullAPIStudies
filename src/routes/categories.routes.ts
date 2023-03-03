f168557db857b7cabe8928d5a1c8621ef2591573
Dilan Lopez <dilanlopez009@gmail.com>
2023-03-03 15:14:07 -0300

    categories repository

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,20 +1,14 @@
 import { Router, Request, Response } from 'express'
-import { v4 as uuidV4 } from 'uuid'
-import { Category } from '../model/Category'
+import { CategoriesRepository } from '../repositories/CategoriesRepository'
 const categoriesRoutes = Router()
+const categoriesRepository = new CategoriesRepository()
 
-const categories: Category[] = []
 categoriesRoutes.post('/categories', (req: Request, res: Response) => {
   const { name, description } = req.body
 
-  const category: Category = {
-    name,
-    description,
-    created_at: new Date()
-  }
-  categories.push(category)
+  categoriesRepository.create({ name, description })
 
-  return res.status(201).json(categories).send()
+  return res.status(201).send()
 })
 
 export { categoriesRoutes }
