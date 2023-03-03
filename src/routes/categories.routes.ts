189d9412934ff4ecb5ce2fd2fa9d170a179ad621
Dilan Lopez <dilanlopez009@gmail.com>
2023-03-03 14:53:31 -0300

    add id in constructor

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,15 +1,18 @@
 import { Router, Request, Response } from 'express'
 import { v4 as uuidV4 } from 'uuid'
+import { Category } from '../model/Category'
 const categoriesRoutes = Router()
 
-const categories = []
+const categories: Category[] = []
 categoriesRoutes.post('/categories', (req: Request, res: Response) => {
   const { name, description } = req.body
-  categories.push({
-    id: uuidV4(),
+
+  const category: Category = {
     name,
-    description
-  })
+    description,
+    created_at: new Date()
+  }
+  categories.push(category)
 
   return res.status(201).json(categories).send()
 })
