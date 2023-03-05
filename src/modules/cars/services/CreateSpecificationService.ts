dd09e7d0784ad0f01c3085b94ce924c939dd70e3
Dilan Lopez <dilanlopez009@gmail.com>
2023-03-05 20:13:52 -0300

    Specificaion Service

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,4 +1,16 @@
+import { ISpecificationRepository } from '../repositories/ISpecificationRepository'
+
+interface IRequest {
+  name: string
+  description: string
+}
 class CreateSpecificationService {
-  execute() {}
+  constructor(private specificationsRepository: ISpecificationRepository) {}
+  execute({ name, description }: IRequest): void {
+    this.specificationsRepository.create({
+      name,
+      description
+    })
+  }
 }
 export { CreateSpecificationService }
