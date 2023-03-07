942747ef31b2b4099d73eab31ad0950b201e866f
Dilan Lopez <dilanlopez009@gmail.com>
2023-03-07 10:33:33 -0300

    Singleton Pattern

--- src/modules/cars/useCases/CreateCategory/index.ts
+++ src/modules/cars/useCases/CreateCategory/index.ts
@@ -1,7 +1,7 @@
 import { CategoriesRepository } from '../../repositories/CategoriesRepository'
 import { CreateCategoryController } from './CreateCategoryController'
 import { CreateCategoryService } from './CreateCategoryUseCase'
-const categoriesRepository = new CategoriesRepository()
+const categoriesRepository = CategoriesRepository.getInstance()
 const createCategoryUseCase = new CreateCategoryService(categoriesRepository)
 
 const createCategoryController = new CreateCategoryController(
